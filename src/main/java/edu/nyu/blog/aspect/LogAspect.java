ec088aeb8062d625f36ce302a8b0ec262fea043a
Naisheng Zhang <jonathan.n.zhang@gmail.com>
2020-06-04 21:22:52 -0400

    add logging

---
+++ src/main/java/edu/nyu/blog/aspect/LogAspect.java
@@ -0,0 +1,73 @@
+package edu.nyu.blog.aspect;
+
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("execution(* edu.nyu.blog.web.*.*(..))")
+    public void log() {
+    }
+
+    @Before("log()")
+    public void deBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURL().toString();
+        String ip = request.getRemoteAddr();
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        RequestLog requestLog = new RequestLog(url,ip,classMethod,args);
+
+        logger.info("Request: {}", requestLog);
+    }
+
+    @After("log()")
+    public void doAfter() {
+//        logger.info("-----doAfter-----");
+    }
+
+    @AfterReturning(returning = "result", pointcut = "log()")
+    public void doAfterReturn(Object result) {
+        logger.info("result: " + result);
+    }
+
+    private class RequestLog {
+        private String url;
+        private String ip;
+        private String classMethod;
+        private Object[] args;
+
+        public RequestLog(String url, String ip, String classMethod, Object[] args) {
+            this.url = url;
+            this.ip = ip;
+            this.classMethod = classMethod;
+            this.args = args;
+        }
+
+        @Override
+        public String toString() {
+            return "RequestLog{" +
+                    "url='" + url + '\'' +
+                    ", ip='" + ip + '\'' +
+                    ", classMethod='" + classMethod + '\'' +
+                    ", args=" + Arrays.toString(args) +
+                    '}';
+        }
+    }
+}
--- src/main/java/edu/nyu/blog/web/IndexController.java
+++ src/main/java/edu/nyu/blog/web/IndexController.java
@@ -3,16 +3,18 @@ package edu.nyu.blog.web;
 import edu.nyu.blog.NotFoundExcepiton;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index() {
-        int i = 9 / 0;
-        String blog = null;
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
+//        int i = 9 / 0;
+//        String blog = null;
 //        if (blog == null) {
 //            throw new NotFoundExcepiton("blog not exit");
 //        }
+        System.out.println("--------index--------");
         return "index";
     }
 }
