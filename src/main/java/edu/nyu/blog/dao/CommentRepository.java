6802780097d6a7b7d09d74cbe5c4004f2e46f632
Naisheng Zhang <jonathan.n.zhang@gmail.com>
2020-06-09 18:38:32 -0400

    add comment chains

--- src/main/java/edu/nyu/blog/dao/CommentRepository.java
+++ src/main/java/edu/nyu/blog/dao/CommentRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findByBlogId(Long blogId, Sort sort);
+    List<Comment> findByBlogIdAndParentCommentNull(Long blogId, Sort sort);
 
 
 }
--- src/main/java/edu/nyu/blog/service/CommentServiceImpl.java
+++ src/main/java/edu/nyu/blog/service/CommentServiceImpl.java
@@ -2,11 +2,13 @@ package edu.nyu.blog.service;
 
 import edu.nyu.blog.dao.CommentRepository;
 import edu.nyu.blog.po.Comment;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -19,7 +21,8 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public List<Comment> listCommentByBlogId(Long blogId) {
         Sort sort = new Sort(Sort.Direction.DESC, "createTime");
-        return commentRepository.findByBlogId(blogId, sort);
+        List<Comment> comments = commentRepository.findByBlogIdAndParentCommentNull(blogId, sort);
+        return eachComment(comments);
     }
 
     @Transactional
@@ -33,5 +36,65 @@ public class CommentServiceImpl implements CommentService {
         }
         comment.setCreateTime(new Date());
         return commentRepository.save(comment);
+    }
+
+
+    /**
+     * go through each top comment node
+     *
+     * @param comments
+     * @return
+     */
+    private List<Comment> eachComment(List<Comment> comments) {
+        List<Comment> commentsView = new ArrayList<>();
+        for (Comment comment : comments) {
+            Comment c = new Comment();
+            BeanUtils.copyProperties(comment, c);
+            commentsView.add(c);
+        }
+        //合并评论的各层子代到第一级子代集合中
+        combineChildren(commentsView);
+        return commentsView;
+    }
+
+    /**
+     * @param comments root根节点，blog不为空的对象集合
+     * @return
+     */
+    private void combineChildren(List<Comment> comments) {
+
+        for (Comment comment : comments) {
+            List<Comment> replys1 = comment.getReplyComments();
+            for (Comment reply1 : replys1) {
+                //循环迭代，找出子代，存放在tempReplys中
+                recursively(reply1);
+            }
+            //修改顶级节点的reply集合为迭代处理后的集合
+            comment.setReplyComments(tempReplys);
+            //清除临时存放区
+            tempReplys = new ArrayList<>();
+        }
+    }
+
+    //存放迭代找出的所有子代的集合
+    private List<Comment> tempReplys = new ArrayList<>();
+
+    /**
+     * 递归迭代，剥洋葱
+     *
+     * @param comment 被迭代的对象
+     * @return
+     */
+    private void recursively(Comment comment) {
+        tempReplys.add(comment);//顶节点添加到临时存放集合
+        if (comment.getReplyComments().size() > 0) {
+            List<Comment> replys = comment.getReplyComments();
+            for (Comment reply : replys) {
+                tempReplys.add(reply);
+                if (reply.getReplyComments().size() > 0) {
+                    recursively(reply);
+                }
+            }
+        }
     }
 }
