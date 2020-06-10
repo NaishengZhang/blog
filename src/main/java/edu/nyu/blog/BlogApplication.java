d4e5f5fc4d81fa0f421ae0687ba0e763caac140b
Naisheng Zhang <jonathan.n.zhang@gmail.com>
2020-06-10 19:44:20 -0400

    deploy on local tomcat

--- src/main/java/edu/nyu/blog/BlogApplication.java
+++ src/main/java/edu/nyu/blog/BlogApplication.java
@@ -2,12 +2,19 @@ package edu.nyu.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 }
