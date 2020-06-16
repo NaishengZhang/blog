a0cc683b586a284b9a592ad61d7d53c894d730ae
Naisheng Zhang <jonathan.n.zhang@gmail.com>
2020-06-15 23:58:55 -0400

    use jar to deploy

--- src/main/java/edu/nyu/blog/BlogApplication.java
+++ src/main/java/edu/nyu/blog/BlogApplication.java
@@ -2,19 +2,17 @@ package edu.nyu.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication extends SpringBootServletInitializer {
+public class BlogApplication /*extends SpringBootServletInitializer*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
 
-    @Override
+/*    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return super.configure(builder);
-    }
+    }*/
 }
